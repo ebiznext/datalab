c41ec8b833c8128f59fe10cd16529a888115c753
Stéphane Manciot <stephane.manciot@ebiznext.com>
2016-01-06 13:39:31 +0100

    add flume monitoring

--- datalab/roles/docker.flume/templates/start.sh
+++ datalab/roles/docker.flume/templates/start.sh
@@ -8,4 +8,8 @@ if [[ -z "$FLUME_CONFIG_FILE" ]]; then
     export FLUME_CONFIG_FILE={{flume_config_dir}}/flume-conf.properties
 fi
 
-{{flume_home_dir}}/bin/flume-ng agent -n $FLUME_AGENT_NAME -c {{flume_config_dir}} -f $FLUME_CONFIG_FILE
+if [[ -z "$FLUME_LOG_DIR" ]]; then
+    export FLUME_LOG_DIR={{flume_log_dir}}
+fi
+
+{{flume_home_dir}}/bin/flume-ng agent -n $FLUME_AGENT_NAME -c {{flume_config_dir}} -f $FLUME_CONFIG_FILE -Dflume.monitoring.type=http -Dflume.monitoring.port=41414 >>${FLUME_LOG_DIR}/flume.${FLUME_AGENT_NAME}.init.log 2>&1
