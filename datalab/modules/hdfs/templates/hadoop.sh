5ac8aca3c85a2d2aa724ea808df3f93408299e7c
Stéphane Manciot <stephane.manciot@ebiznext.com>
2016-02-05 15:37:35 +0100

    add hdfs role

---
+++ datalab/modules/hdfs/templates/hadoop.sh
@@ -0,0 +1,6 @@
+export HADOOP_HOME={{hdfs.home_dir}}
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+export JAVA_HOME=/usr/java/default
+export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
